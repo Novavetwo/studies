03baf740af3fb0e9b6a67adc827cdffbee354dab
Novavetwo <88737299+Novavetwo@users.noreply.github.com>
2024-07-25 22:30:00 -0300

    Add files via upload

--- tiquete_ru.py
+++ tiquete_ru.py
@@ -18,8 +18,8 @@ class Usuario(Enum):
     ALUNO = auto()
     DOCENTE = auto()
     EXTERNO = auto()
-    SERVIDOR_TRES = auto()
-    TRES_SERVIDOR = auto()
+    SERVIDOR_MAIS_QUE_TRES = auto()
+    SERVIDOR_MENOS_QUE_TRES = auto()
 
 class Pagamento(Enum):
     '''
@@ -39,7 +39,7 @@ def tipo_usuario(n: int) -> Usuario:
     >>> tipo_usuario(0)
     <Usuario.ALUNO: 1>
     '''
-    usuarios: list[Usuario] = [Usuario.ALUNO, Usuario.DOCENTE, Usuario.EXTERNO, Usuario.SERVIDOR_TRES, Usuario.TRES_SERVIDOR]
+    usuarios: list[Usuario] = [Usuario.ALUNO, Usuario.DOCENTE, Usuario.EXTERNO, Usuario.SERVIDOR_MAIS_QUE_TRES, Usuario.SERVIDOR_MENOS_QUE_TRES]
     return usuarios[n]
 
 def forma_pagamento(n: int) -> Pagamento:
@@ -59,7 +59,7 @@ def feedback_usuario(entrada: int, categoria: str) -> str:
     Retorna um feedback de qual número o usuário selecionou e o que ele representa. 
     Feito através de operações lógicas.
     Exemplos:
-    >>> feedback_usuario(1, 'Usuario')
+    >>> feedback_usuario(1, 'Usuário')
     'Você selecionou 1 - ALUNO'
     >>> feedback_usuario(1, 'Pagamento')
     'Você selecionou 1 - DINHEIRO'
@@ -68,9 +68,9 @@ def feedback_usuario(entrada: int, categoria: str) -> str:
     >>> feedback_usuario(1, 'Tíquetes')
     'Você selecionou 1 - Tíquete'
     >>> feedback_usuario(0, 'Tíquetes')
-    'Você selecinou 0 - Quantidade inválida para Tíquetes'
+    'Você selecionou 0 - Quantidade inválida para Tíquetes'
     '''
-    if categoria == 'Usuario':
+    if categoria == 'Usuário':
         resposta = tipo_usuario(entrada - 1).name
     elif categoria == 'Tíquetes':
         if entrada > 1:
@@ -83,11 +83,22 @@ def feedback_usuario(entrada: int, categoria: str) -> str:
         resposta = forma_pagamento(entrada - 1).name
     frase = 'Você selecionou ' + str(entrada) + ' - ' + resposta
     return frase
+def continuar_ou_nao(escolha: str) -> bool:
+    '''
+    Esta função permite ao usuário decidir se irá ou não continuar seu processo,
+    ou se deseja colocar uma nova entrada à função input anterior. Isso é possível
+    através de um input definido como resposta positiva ou negativa, o que resultará
+    em um loop sendo efetuado ou descontinuado
+    Exemplos:
+    >>> continuar_ou_nao
+    '''
 
 def registrar_venda() -> None:
     '''
     O registro de venda recebe o tipo de usuário, quantos tíquetes estão sendo comprados e
-    a forma de pagamento. Após, apresenta o valor total da venda e, após a confirmação do operador,
+    a forma de pagamento. Em cada um desses recebimentos, mostra ao usuário as opções 
+    disponíveis e, também, o que ele está escolhendo, para que possa corrigir, caso 
+    necessário. Após, apresenta o valor total da venda e, após a confirmação do operador,
     fica disponível para registrar outra venda.
     '''
     print('Tipo de Usuário')
@@ -98,13 +109,15 @@ def registrar_venda() -> None:
     print('》 5 - Servidores Públicos com mais de 3 salários mínimos')
     entrada_tipo_usuario_str: str = input('》 ')
     entrada_tipo_usuario: int = int(entrada_tipo_usuario_str)
-    categoria = 'Usuario'
+    categoria = 'Usuário'
     print(feedback_usuario(entrada_tipo_usuario, categoria))
 
     print("Quantidade de tíquetes")
     q_de_tiquetes_str: str = input("》 ")
+    q_de_tiquetes: int = int(q_de_tiquetes_str)
     categoria = 'Tíquetes'
-    q_de_tíquetes: int = int(q_de_tiquetes_str)
+    print(feedback_usuario(q_de_tiquetes, categoria))
+
     print('Forma de pagamento')
     print('》 1 - Dinheiro')
     print('》 2 - PIX')
@@ -112,13 +125,55 @@ def registrar_venda() -> None:
     entrada_forma_pagamento_str: str = input('》 ')
     entrada_forma_pagamento: int = int(entrada_forma_pagamento_str)
     categoria = 'Pagamento'
-    feedback_usuario(entrada_forma_pagamento, categoria)
-
+    print(feedback_usuario(entrada_forma_pagamento, categoria))
     
+    resumo = resumo_de_registro(entrada_tipo_usuario, q_de_tiquetes, entrada_forma_pagamento)
+    print(resumo[0])
+    print(resumo[1])
+    print(resumo[2])
+
+def resumo_de_registro(entrada_tipo_usuario: int, q_de_tiquetes: int, entrada_forma_pagamento: int) -> list[str]:
+    '''
+    Monta uma lista com o resumo de uma venda. Este resumo contém o tipo de usuário, 
+    a quantidade de tíquetes vendidos e a forma de pagamento utilizada. O processo é 
+    feito através de operações de listas e funções externas.
+    A quantidade de tíquetes deve ser positiva.
+    Exemplos:
+    >>> monta_lista_de_registro(1, 1, 1)
+    ['Usuário: ALUNO', 'Tíquete: 1', 'Pagamento: DINHEIRO']
+    >>> monta_lista_de_registro(3, 4, 2)
+    ['Usuário: EXTERNO', 'Tíquetes: 4', 'Pagamento: PIX']
+    '''
+    primeira_linha = 'Usuário: ' + tipo_usuario(entrada_tipo_usuario - 1).name
+    if q_de_tiquetes > 1:
+        segunda_linha = 'Tíquetes: ' + str(q_de_tiquetes)
+    elif q_de_tiquetes == 1:
+        segunda_linha = 'Tíquete: ' + str(q_de_tiquetes)
+    terceira_linha = 'Pagamento: ' + forma_pagamento(entrada_forma_pagamento - 1).name
+    lst: list[str] = [primeira_linha, segunda_linha, terceira_linha]
+    return lst
+
+def monta_lista_de_registro(entrada_tipo_usuario: int, q_de_tiquetes: int, entrada_forma_pagamento: int) -> Registro:
+    '''
+    Monta uma lista com o resumo de uma venda. Este resumo contém o tipo de usuário, 
+    a quantidade de tíquetes vendidos e a forma de pagamento utilizada. O processo é 
+    feito através de operações de listas e funções externas.
+    A quantidade de tíquetes deve ser positiva.
+    Exemplos:
+    >>> monta_lista_de_registro(1, 1, 1)
+    ['Usuário: ALUNO', 'Tíquete: 1', 'Pagamento: DINHEIRO']
+    >>> monta_lista_de_registro(3, 4, 2)
+    ['Usuário: EXTERNO', 'Tíquetes: 4', 'Pagamento: PIX']
+    '''
+    usuario = tipo_usuario(entrada_tipo_usuario - 1).name
+    tiquetes = q_de_tiquetes
+    pagamento = forma_pagamento(entrada_forma_pagamento - 1).name
+    lst: Registro = Registro(usuario, tiquetes, pagamento)
+    return lst
+
 def relatorio_vendas():
     '''
-    
-    
+
     '''
 
     return 0
